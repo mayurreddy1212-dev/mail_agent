7d9b47bd3725fb260ff438268c81e41da90ec52e
Mayur <mayurreddy1212@gmail.com>
2026-02-14 19:56:33 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from app.database import engine
 from app.routers import auth,employees
 from app.models import Base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 async def root():
